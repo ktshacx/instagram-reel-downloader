83c28b3b78fd359f9bed40f699da7814b183f82c
KTS Hacx <69856755+ktshacx@users.noreply.github.com>
2023-09-11 01:08:05 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,12 +12,7 @@ export default function HomePage() {
       />
       <main className="mx-auto w-full flex-1 rounded p-4">
         <InstagramForm />
-        <p className="my-4 text-center text-sm text-gray-400 motion-safe:animate-[animate-late-fade-in_2s_ease-in-out_1] dark:text-gray-400">
-          If the download opens a new page, just right click the video and then
-          click `Save as video`
-        </p>
       </main>
-      <Footer />
     </div>
   );
 }
