094e02bdba9470b34a116ebbbe080aa46dd17395
KTS Hacx <69856755+ktshacx@users.noreply.github.com>
2023-09-11 01:11:06 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,6 @@ import InstagramForm from "@/components/InstagramForm";
 export default function HomePage() {
   return (
     <div className="container mx-auto flex min-h-screen flex-col justify-between py-8 md:max-w-3xl">
-      <Header
-        title="Instagram Video Downloader"
-        subtitle="Download Instagram Videos for Free"
-        className="mb-24 flex h-fit w-full flex-col items-center justify-center text-center"
-      />
       <main className="mx-auto w-full flex-1 rounded p-4">
         <InstagramForm />
       </main>
