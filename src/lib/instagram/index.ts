2fea6e110768fb4c8c5c68ab489c0ad3c283a6fb
KTS Hacx <69856755+ktshacx@users.noreply.github.com>
2023-09-11 23:12:44 +0530

    Update index.ts

--- src/lib/instagram/index.ts
+++ src/lib/instagram/index.ts
@@ -35,13 +35,17 @@ export const fetchPostJson = async (postID: string, timeout?: number) => {
   const postUrl = "https://www.instagram.com/p/" + postID;
 
   const pageJson = await fetchFromPage(postUrl, timeout);
-  if (pageJson) return pageJson;
-
-  const apiJson = await fetchFromAPI(postUrl, timeout);
-  if (apiJson) return apiJson;
-
-  throw new BadRequest(
+  if (pageJson) {
+    return pageJson;
+  }else{
+    const apiJson = await fetchFromAPI(postUrl, timeout);
+    if (apiJson) {
+      return apiJson;
+    }else{
+      throw new BadRequest(
     "The video download link for this post is not available.",
     401
   );
+    }
+  }
 };
